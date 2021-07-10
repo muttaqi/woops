7c3ca59dd750eb5bbe40677ed81b0fe1c58fddf4
Muttaqi <39924187+muttaqi@users.noreply.github.com>
2021-07-09 21:31:27 -0700

    Documentation

--- woops_test.m
+++ woops_test.m
@@ -3,30 +3,39 @@ Import["woops.m"]
 On[Assert]
 
 c := Class[
+    (* variables *)
     <|
         "a"->0
     |>,
+    (* functions *)
     <|
+        (* function name as string *)
         "add1" -> Function[
+            (* arguments *)
             {x},
+            (* output *)
             x + 1
         ],
+        (* private function, underscore prefixed *)
         "_private" -> Function[
             {x},
             x + 1
         ],
         "add1ToA" -> Function[
+            (* class variable access *)
             {this},
             this["a"] + 1
         ],
         "mutateAndReturnTrue" -> Function[
             {this},
             (
+                (* mutation simulated in a functional style *)
                 out = this["a", 4];
                 {out["__ToInstance__"], True}
             )
         ],
         "callPrivate" -> Function[
+            (* calling a private function *)
             {this},
             this["_private", {2}]
         ]
@@ -35,10 +44,13 @@ c := Class[
 
 o := New[c, <|"a"->1|>]
 
+(* variable access *)
 Assert[o["a"] == 1]
+(* function call, arguments in a list *)
 Assert[o["add1", {2}] == 3]
 Assert[o["add1ToA", {}] == 2]
 
+(* variable mutation *)
 o = o["a", 3]
 Assert[o["a"] == 3]
 
@@ -47,9 +59,12 @@ Assert[res[[2]]]
 o = res[[1]]
 Assert[o["a"] == 4]
 
+(* calling private function outside of class *)
 Assert[Catch[o["_private", {4}]] == "Unknown function _private"]
+(* calling private function through intermediate function *)
 Assert[o["callPrivate", {}] == 3]
 
+(* subclass *)
 subC := Extend[c,
     <|
         "a" -> 2,
@@ -66,6 +81,7 @@ subC := Extend[c,
         "callSuperMutateAndReturnTrue" -> Function[
             {this},
             (
+                (* accessing super-class's function *)
                 this["_super"]["mutateAndReturnTrue", {}]
             )
         ]
